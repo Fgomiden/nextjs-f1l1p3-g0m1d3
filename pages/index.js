e5a4f2e6961b03630d1c9867851ca180c8f0b7e2
Fgomiden <fgomiden@gmail.com>
2020-11-26 16:30:00 -0300

    get personagens

--- pages/index.js
+++ pages/index.js
@@ -1,127 +1,65 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import axios from "axios";
 import Nav from "../components/nav";
 import Cartao from "../components/cartao";
+import Link from "next/link";
 
 export default function IndexPage() {
   const [filmes, setFilmes] = useState([]);
 
   const getMovies = async () => {
-    try {
-      const response = await axios.get(
-        `https://swapi.dev/api/films/?format=json`
-      );
-      setFilmes(response.data);
-      //console.log(JSON.stringify(response.data));
-    } catch (error) {
-      // handle error
-      console.log(error.message);
-    }
+    const response = await axios
+      .get(`https://swapi.dev/api/films/?format=json`)
+      .catch((err) => console.log("Erro:", err));
+    setFilmes(response.data.results);
   };
-  // console.log(filmes);
+
   useEffect(() => {
     getMovies();
   }, []);
-  const Movies = [
-    {
-      id: 1,
-      title: "A New Hope",
-      episode_id: 4,
-      director: "George Lucas",
-      producer: "Gary Kurtz, Rick McCallum",
-      release_date: "1977-05-25",
-      image: "https://images-na.ssl-images-amazon.com/images/I/81aA7hEEykL.jpg",
-    },
-    {
-      id: 2,
-      title: "The Empire Strikes Back",
-      episode_id: 5,
-      director: "Irvin Kershner",
-      producer: "Gary Kurtz, Rick McCallum",
-      release_date: "1977-05-25",
-      image: "https://images-na.ssl-images-amazon.com/images/I/81aA7hEEykL.jpg",
-    },
-    {
-      id: 3,
-      title: "Return of the Jedi",
-      episode_id: 6,
-      director: "Richard Marquand",
-      producer: "Gary Kurtz, Rick McCallum",
-      release_date: "1977-05-25",
-      image: "https://images-na.ssl-images-amazon.com/images/I/81aA7hEEykL.jpg",
-    },
-    {
-      id: 4,
-      title: "Return of the Jedi",
-      episode_id: 6,
-      director: "Richard Marquand",
-      producer: "Gary Kurtz, Rick McCallum",
-      release_date: "1977-05-25",
-      image: "https://images-na.ssl-images-amazon.com/images/I/81aA7hEEykL.jpg",
-    },
-    {
-      id: 5,
-      title: "Return of the Jedi",
-      episode_id: 6,
-      director: "Richard Marquand",
-      producer: "Gary Kurtz, Rick McCallum",
-      release_date: "1977-05-25",
-      image: "https://images-na.ssl-images-amazon.com/images/I/81aA7hEEykL.jpg",
-    },
-    {
-      id: 6,
-      title: "Return of the Jedi",
-      episode_id: 6,
-      director: "Richard Marquand",
-      producer: "Gary Kurtz, Rick McCallum",
-      release_date: "1977-05-25",
-      image: "https://images-na.ssl-images-amazon.com/images/I/81aA7hEEykL.jpg",
-    },
-  ];
+  //console.log("Filmes:", filmes);
+
+  const router = useRouter();
+  console.log(router);
+
   return (
-    <div className="bg-black flex flex-col items-center justify-center">
-      {Movies.map((item) => (
+    <div className=" flex flex-col  items-center justify-center">
+      {filmes.map((item, id) => (
         <div
-          className="w-full h-40"
-          key={item.id}
+          style={{ width: 500 }}
+          className="flex m-5"
+          key={id}
           //styles={{ backgroundImage: `url(/sw4.jpg)` }}
         >
-          <div
-            className="flex flex-1 w-full h-full"
-            styles={{ backgroundColor: "rgba(0,0,0,0.6)" }}
-          >
-            <div className="flex flex-1">
-              <Image
-                src="/sw4.jpg"
-                alt="Picture of the author"
-                width={300}
-                height={300}
-                layout="responsive"
-              />
-            </div>
-            <div className="flex flex-1">
-              <div className="px-4 py-2">
-                <div className="text-2xl text-white font-bold">
-                  Star Wars {item.episode_id}: {item.title}
-                </div>
-                <div className="text-sm text-white">
-                  Diretor: {item.director}
-                </div>
-                <div className="text-sm text-white">
-                  Produtor: {item.producer}
-                </div>
-                <div className="text-sm text-white">
-                  Lançamento: {item.release_date}
-                </div>
-                <div className="mb-2">
-                  <a
-                    href="/Personagens"
-                    className="font-bold leading-tight text-sm text-gray-100 hover:text-gray-100"
-                  >
+          <div className="flex flex-1 bg-yellow-200 items-center justify-center">
+            <Image
+              src="/sw4.jpg"
+              alt="Picture of the author"
+              width={200}
+              height={200}
+              className="object-contain"
+            />
+          </div>
+          <div className="flex flex-1 bg-yellow-700">
+            <div className="px-4 py-2">
+              <div className="text-xl text-white font-bold">
+                Star Wars {item.episode_id}: {item.title}
+              </div>
+              <div className="text-sm text-white">Diretor: {item.director}</div>
+              <div className="text-sm text-white">
+                Produtor: {item.producer}
+              </div>
+              <div className="text-sm text-white">
+                Lançamento: {item.release_date}
+              </div>
+              <div className="mb-2">
+                <Link href={`/personagens`}>
+                  <a className="font-bold leading-tight text-sm text-gray-100 hover:text-gray-100">
                     Ver Personagens
                   </a>
-                </div>
+                </Link>
               </div>
             </div>
           </div>
@@ -135,5 +73,7 @@ export default function IndexPage() {
   /* <Cartao 
   titulo={item.title} 
   imagem={item.image}
-/> */
+/> 
+
+${encodeURIComponent(item.id)}*/
 }
--- pages/personagens.js
+++ pages/personagens.js
@@ -1,30 +1,65 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useRouter } from "next/router";
+import Image from "next/image";
 import Nav from "../components/nav";
 import Cartao from "../components/cartao";
 
-export default function IndexPage() {
-  const [filmes, setFilmes] = useState([]);
-  
-  export const getMovie = async (id) => {
-    const { data } = await axios.get(`https://swapi.dev/api/films/${id}/`);
-    return data;
+export default function Personagens() {
+  const [personagens, setPersonagens] = useState([]);
+  //async (id)
+  //people/${id}/
+  const getPeople = async () => {
+    const response = await axios
+      .get(`https://swapi.dev/api/people/?format=json`)
+      .catch((err) => console.log("Erro:", err));
+    setPersonagens(response.data.results);
   };
-
-  export const getPeople = async (id) => {
-    const { data } = await axios.get(
-      `https://swapi.dev/api/people/${id}/?format=json`
-    );
-    return data;
-  };
-
-
+  useEffect(() => {
+    getPeople();
+  }, []);
+  console.log("personagnes:", personagens);
   return (
     <div>
-      <Nav />
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
-        <Cartao titulo={item.title} imagem={item.image} />
-      </div>
+      {/* <Nav /> */}
+      {personagens.map((item, id) => (
+        <div
+          style={{ width: 500 }}
+          className="flex m-5"
+          key={id}
+          //styles={{ backgroundImage: `url(/sw4.jpg)` }}
+        >
+          <div className="flex flex-1 bg-yellow-200 items-center justify-center">
+            <Image
+              src="/sw4.jpg"
+              alt="Picture of the author"
+              width={200}
+              height={200}
+              className="object-contain"
+            />
+          </div>
+          <div className="flex flex-1 bg-yellow-700">
+            <div className="px-4 py-2">
+              <div className="text-xl text-white font-bold">{item.name}</div>
+              <div className="text-sm text-white">Altura: {item.height}</div>
+              <div className="text-sm text-white">Massa: {item.mass}</div>
+              <div className="text-sm text-white">
+                Cor do Cabelo: {item.hair_color}
+              </div>
+              <div className="text-sm text-white">
+                Cor da Pele: {item.skin_color}
+              </div>
+              <div className="text-sm text-white">
+                Cor dos Olhos: {item.eye_color}
+              </div>
+              <div className="text-sm text-white">
+                Nascimento: {item.birth_year}
+              </div>
+              <div className="text-sm text-white">Genero: {item.gender}</div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
